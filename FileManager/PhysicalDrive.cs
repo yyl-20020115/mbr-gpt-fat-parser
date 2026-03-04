ca877f17554d8d9d0736d6175aafa1414f5392de
杨铸人 <49382681@qq.com>
2026-03-04 15:03:09 +0800

    fx

--- FileManager/PhysicalDrive.cs
+++ FileManager/PhysicalDrive.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Win32.SafeHandles;
 using System.IO;
 
@@ -33,33 +31,67 @@ class PhysicalDrive
         if (shFile.IsInvalid)
             throw new InvalidDataException("Reading data form " + driveName + " is not avalible");
 
-        //---------------------------------------------------
-        MBR protectiveMbr = new MBR(buff);
-
-        if (protectiveMbr.isProtective())
+        List<(uint, uint, string)> partitions = [];
+        for (uint i = 1; ; i++)
         {
-            Scanner = new GPTScan(name);
-        }
-        else
-        {
-            Scanner = new MBRScan(name);
-        }
-        //---------------------------------------------------
+            WinApi.SetFilePointer(shFile, i * SectorSize, out var high, 0);
 
-        //---------------------------------------------------
-        for (int i = 0; i < Scanner.Length; i++)
-        {
-            volumes.Add(new Volume(driveName, Scanner[i]));
+            WinApi.ReadFile(shFile, buff, SectorSize, out nBytesRead, (IntPtr)0);
+            if (shFile.IsInvalid)
+                throw new InvalidDataException("Reading data form " + driveName + " is not avalible");
+            var signature0 = BitConverter.ToUInt32(buff, 0);
+            if (signature0 == 0) break;
+            var signature1 = BitConverter.ToUInt32(buff, 4);
+            var offset = BitConverter.ToUInt32(buff, 8) * SectorSize;
+            var length = BitConverter.ToUInt32(buff, 12) * SectorSize;
+            var name = Encoding.ASCII.GetString(buff, 16, (int)SectorSize - 16).TrimEnd('\0');
+            partitions.Add((offset, length, name));
+            Console.WriteLine($"{name}: offset={offset:X8}, length={length:X8}");
         }
-        //---------------------------------------------------
+        foreach (var partition in partitions)
+        {
+            using var stream = new FileStream(partition.Item3 + ".bin", FileMode.Create, FileAccess.Write);
+            WinApi.SetFilePointer(shFile, partition.Item1, out var high, 0);
+            for (uint i = 0; i < partition.Item2; i += SectorSize)
+            {
+                WinApi.ReadFile(shFile, buff, SectorSize, out nBytesRead, (IntPtr)0);
+                stream.Write(buff, 0, (int)SectorSize);
+            }
+        }
 
+
+
+
+        if (false)
+        {
+
+            //---------------------------------------------------
+            MBR protectiveMbr = new MBR(buff);
+
+            if (protectiveMbr.isProtective())
+            {
+                Scanner = new GPTScan(name);
+            }
+            else
+            {
+                Scanner = new MBRScan(name);
+            }
+            //---------------------------------------------------
+
+            //---------------------------------------------------
+            for (int i = 0; i < Scanner.Length; i++)
+            {
+                volumes.Add(new Volume(driveName, Scanner[i]));
+            }
+            //---------------------------------------------------
+        }
         shFile.Close();
     }
     //------------------------------------------------------------------------- ~PhysicalDrive(string driveName)
 
     public Volume this[int index]
     {
-        get 
+        get
         {
             try
             {
@@ -76,12 +108,12 @@ class PhysicalDrive
     {
         get
         {
-            
+
             foreach (Volume v in volumes)
             {
                 //compare only letter
-                if ( v.Name[0] == volumeName[0] )
-                    return v;            
+                if (v.Name[0] == volumeName[0])
+                    return v;
             }
             throw new VolumeNotFoundException("Volume " + volumeName + " not found");
         }
@@ -95,7 +127,7 @@ class PhysicalDrive
 
     //public bool isOneOf("C:\");
 
-   
+
 
 
 }
